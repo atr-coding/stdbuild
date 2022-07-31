c54a3effa280c519193e827fdeeffe6cbfa9e67c
Austin <23746827+atr-coding@users.noreply.github.com>
2022-07-31 15:29:13 -0500

    Refreshed using gitignore.

--- module/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "test.h"
-
-#include <iostream>
-
-void test_func() {
-	std::cout << "test_func()" << '\n';
-}
--- project/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <test.h>
-
-int main() {
-	test_func();
-#ifdef TEST_DEF
-	std::cout << "TEST_DEF is defined.\n";
-#endif
-}
