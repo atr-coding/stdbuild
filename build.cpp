7210f915b2341fbb459e1cef972fe9ce428d4ba8
Austin <23746827+atr-coding@users.noreply.github.com>
2022-07-30 19:06:27 -0500

    Improved Lists. Added verbosity control.

--- build.cpp
+++ build.cpp
@@ -1,51 +1,36 @@
-#include <iostream>
-#include <vector>
-#include <concepts>
 #include "stdbuild"
 
 #include "module/build.h"
 
 using namespace std::build;
 
-int main(int argc, char** argv) {
+int main() {
+	enable_debugging();
+	auto module = module::build();
 
 	List sources = {
-		"src/main.cpp",
-		"src/test.cpp"
+		"project/src/main.cpp",
+		"project/src/test.cpp"
 	};
 
-	// enable_debugging();
-
-	// auto module = module::build();
-
-	// std::vector<std::string> files = { "src/main.cpp", "src/test.cpp" };
-
-	// project proj("project", type::executable);
-	// set_directory(proj, "project");
-	// add_flags(proj, "-std=c++20", "-DTEST_DEF");
-	// add_include_directories(proj, "include/");
-	// add_source_files(proj, files);
-	// add_dependencies(proj, module);
-	// set_version(proj, { 0, 0, 1 });
-	// create_executable(proj);
-
-
-	/*
-
-	IncludeDirectoryList incs = {
-		{"include/", true}
+	List flags = {
+		"-std=c++20",
+		"-DTEST_DEF"
 	};
 
-	proj.add(IncludeDirectory("include/", true), Flags("-std=c++20", "-DTEST_DEF"), Files(files), Package(module), Library("OpenGL32.lib", true));
-	proj.set_version({0, 0, 1});
+	List inc_dirs = {
+		"project/include/"
+	};
 
-	*/
+	// PackageList packages = {
+	// 	module::build()
+	// };
+	
+	project proj("project", type::executable);
+	add_flags(proj, flags);
+	add_include_directories(proj, inc_dirs);
+	add_source_files(proj, sources);
+	add_dependencies(proj, module);
+	set_version(proj, { 0, 0, 1 });
+	create_executable(proj);
 }
-
-
-// std::string expanded_str_vector(const string_vector& str_vec, char delim = 0) {
-// 	std::string output;
-// 	for (const auto& str : str_vec) { output += str + delim; }
-// 	return std::move(output);
-// }
-// build.add_pre_command("cppcheck " + expanded_str_vector(build.source_files(), ' '));
