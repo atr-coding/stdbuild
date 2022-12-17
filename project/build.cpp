5f8190b0289dbedacb5fc0d6b33d0d98109db33c
Austin <23746827+atr-coding@users.noreply.github.com>
2022-12-16 22:39:11 -0600

    Reordered compiler preprocessor directives.

--- project/build.cpp
+++ project/build.cpp
@@ -4,15 +4,17 @@
 #include "../stdbuild"
 #include "packages/packages.h"
 
-const stdbuild::string_list warning_flags = {
-	"-Wall", "-Wextra", "-Wshadow", "-Wconversion", "-Wpedantic", "-Werror"
-};
-
 struct project : stdbuild::package {
 	project() {
 		name = "project";
-		flags = { "-std=c++20", "-DTEST_DEF", "-fanalyzer" };
-		flags += warning_flags;
+		flags = { "-std=c++20", "-DTEST_DEF" };
+
+		if(stdbuild::is_gcc() || stdbuild::is_clang()) {
+			flags += { "-Wall", "-Wextra", "-Wshadow", "-Wconversion", "-Wpedantic", "-Werror" };
+		} else if(stdbuild::is_msvc()) {
+			flags += { "/permissive", "/W4", "/w14640" };
+		}
+
 		sources = { "src/main.cpp", "src/test.cpp" };
 		pkgs = { packages };
 	}
